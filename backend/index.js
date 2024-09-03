64f8126be7d3d8736cfefacbe59b9b39ebf5b007
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-09-03 20:21:27 +0700

    y

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,7 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-// import userRouteV1 from "./routes/userRouteV1.js";  
-// import { connectDB as connectMongoNative } from './database/dbV1.js';
-import userRouteV2 from "./routes/userRouteV2.js";
+import userRoute from "./routes/userRouteV2.js";
 
 const app = express();
 
@@ -16,12 +14,8 @@ const db = mongoose.connection;
 db.on('error', (error) => console.log(error));
 db.once('open', () => console.log('Database berhasil tersambung'));
 
-// connectMongoNative();
-
 app.use(cors());
 app.use(express.json());
-// app.use(userRouteV2);
-// app.use('/api/v1', userRouteV1);
-app.use('/api/v2', userRouteV2);
+app.use(userRoute);
 
 app.listen(5000, () => console.log('Server sedang berjalan'));
