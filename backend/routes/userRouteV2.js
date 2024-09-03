cf0007d66887b74c0348a61918b8483a5af08682
Dzikri Fauzi Ramdhani <dzikrifauziramdhani16@gmail.com>
2024-09-03 13:07:05 +0700

    fix api v2

--- backend/routes/userRouteV2.js
+++ backend/routes/userRouteV2.js
@@ -5,7 +5,7 @@ const router2 = express.Router();
 router2.get('/users/apiv2', getUsersV2);
 router2.get('/users/apiv2/:id', getUserByIdV2);
 router2.post('/users/apiv2', saveUserV2);
-router2.patch('/users/apiv2:id', updateUserV2);
-router2.delete('/users/apiv2:id', deleteUserV2);
+router2.patch('/users/apiv2/:id', updateUserV2);
+router2.delete('/users/apiv2/:id', deleteUserV2);
 
 export default router2;
